ec6ff27a944684d90c201eb8e9ce09063e329f6f
ventur09 <ventura09.10.0@gmail.com>
2022-12-30 15:45:16 -0300

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,35 @@
-var input_fname = document.querySelectorAll('#name');
-function save() {
-    console.log("input_fname: ", input_fname);
+var Responsavel = /** @class */ (function () {
+    function Responsavel() {
+    }
+    return Responsavel;
+}());
+var R = new Array();
+function getValue() {
+    var r = new Responsavel();
+    r.name = document.querySelector('#name')['value'];
+    r.cpf = document.querySelector('#cpf')['value'];
+    save(r);
+}
+function validator(dados) {
+    if (dados.name.length && dados.cpf.length) {
+        return true;
+    }
+    return false;
+}
+function save(r) {
+    if (validator(r)) {
+    }
+    else {
+        alert("É necessário preencher todos os campo para poder continuar!");
+        return;
+    }
+    R.push(r);
+    list();
+    console.log("Saving...", R);
+}
+function list() {
+    for (var _i = 0, R_1 = R; _i < R_1.length; _i++) {
+        var value = R_1[_i];
+        console.log("value: ", value);
+    }
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
-const input_fname = document.querySelectorAll('#name')
-function save() {
-  console.log("input_fname: ", input_fname);
+class Responsavel{
+  name: string
+  cpf: string
+}
+
+var R = new Array()
+
+function getValue(): void{
+  const r = new Responsavel()
+  r.name = document.querySelector('#name')['value']
+  r.cpf = document.querySelector('#cpf')['value']
+  save(r)
+}
+
+function validator(dados: Responsavel): boolean{
+  if(dados.name.length && dados.cpf.length){
+    return true
+  }
+  return false
+}
+function save(r: Responsavel) {
+  if(validator(r)){
+  }else{
+    alert( "É necessário preencher todos os campo para poder continuar!" )
+    return
+  }
+  
+  R.push(r)
+  list()
+
+  console.log("Saving...", R);
+}
+
+function list(){
+
+  for(let value of R){
+    console.log("value: ",value);
+
+  }
 }
